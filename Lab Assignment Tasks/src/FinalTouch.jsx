de0e59554fa5f6f714ec226e78987b8ce48bc360
Zain-The-Coder <zain015976@gmail.com>
2026-01-14 02:51:18 +0500

    React

--- Lab Assignment Tasks/src/FinalTouch.jsx
+++ Lab Assignment Tasks/src/FinalTouch.jsx
@@ -1,7 +1,9 @@
 import Countergame from "./Components/Countergame";
 import Form from "./Components/Form";
+import Layout from "./Components/Layout";
 import PlayGround from "./Components/PlayGround";
 import StudentCard from "./Components/StudentCard";
+import Todo from "./Components/Todo";
 
 function FinalTouch () {
     return (
@@ -19,6 +21,17 @@ function FinalTouch () {
             <h2 className="text-[30px] text-center underline">Level 2 – Forms & State</h2>
             <h2 className="font-bold ml-[20px] mt-[10px]">Task 3: Mini Signup form</h2>
             <Form />
+        </section>
+        <section>
+            <h2 className="font-bold ml-[20px] mt-[10px]">Task 4: Mini Todo Application</h2>
+            <Todo />
+        </section>
+        <section>
+            <h2 className="text-[30px] text-center underline">Level 3 – React In Depth</h2>
+            <h2 className="font-bold ml-[20px] mt-[10px]">Task 3: Component Composition</h2>   
+                <Layout children="This is passed as children to the Layout component."/>
+                    <h2>Main Content Area</h2>
+                    <p>This is passed as children to the Layout component.</p>
         </section>
         </>
     )
