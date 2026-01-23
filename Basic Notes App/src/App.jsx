a453be9db7c6dedfef4be9a45287942593818a04
Zain-The-Coder <zain015976@gmail.com>
2026-01-24 03:48:49 +0500

    Basic Notes App Working

---
+++ Basic Notes App/src/App.jsx
@@ -0,0 +1,55 @@
+import Section from './components/SEction';
+import './App.css'
+import { useState } from 'react';
+
+function App () {
+
+  const [title , setTitle] = useState("");
+  const [des , setDes] = useState("");
+  const [obj , setObj] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let newObj = [...obj];
+    newObj.push({title , des});
+    setObj(newObj);
+    console.log(obj);
+    setTitle("");
+    setDes("");
+  }
+
+  const deleteTask = () => {
+    let userTask = [...obj];
+    console.log();
+    console.log(userTask);
+  }
+
+  return (
+    <>
+    <div className="flex flex-col lg:flex-row h-[100vh]">
+      <section className='p-[40px] bg-black text-white w-full lg:w-1/2'>
+
+      <h2 className='uppercase text-[22px] mb-[10px]'>Add Notes</h2>
+
+      <form onSubmit={handleSubmit} className='w-[100%] flex flex-col gap-[30px] text-white'>
+        <input value={title} onChange={(e) => {setTitle(e.target.value)}} className='border-3 p-[10px] rounded-[10px]' type="text" name='text' placeholder='Enter Notes Heading'/>
+        <textarea value={des} onChange={(e) => {setDes(e.target.value)}} className='border-3 h-[20vh] p-[10px] rounded-[10px]' name="mytextarea" placeholder='Enter Details'></textarea>
+        <button className='p-[10px] bg-white text-black font-bold rounded-[10px] cursor-pointer active:scale-95'>Add Notes</button>
+      </form> 
+
+    </section>
+
+    <section className='p-[40px] bg-black w-full lg:w-1/2 lg:border-l-2 border-white'>
+      <h2 className='uppercase text-[22px] text-white'>Your Notes</h2>
+      <section className='flex flex-wrap justify-start items-start'>
+        {obj.map((item , index) => (
+          <Section key={index} object={item.title} description={item.des} func={deleteTask} />
+        ))}
+      </section>
+    </section>
+    </div>
+    </>
+  )
+}
+
+export default App
